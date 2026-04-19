5587ba909cfd34f766090013c536961fdf18e938
William Babcock <w.babcock@vimiew.com>
2026-04-19 15:38:31 -0400

    bugs

--- inc/kv.h
+++ inc/kv.h
@@ -22,8 +22,8 @@ typedef struct
 
 kv_t *kv_init(size_t capacity);
 int kv_put(kv_t *db, const char *key, const char *value);
-//char *kv_get(kv_t *db, const char *key);
-//int kv_delete(kv_t *db, const char *key);
-//void kv_free(kv_t *db);
+char *kv_get(kv_t *db, const char *key);
+int kv_delete(kv_t *db, const char *key);
+void kv_free(kv_t *db);
 
 #endif
